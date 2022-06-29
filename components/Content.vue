8d59af9727ae16370259872f90a83adeb72b291b
juniantowicaksono06 <juniantowicaksono06@gmail.com>
2022-06-30 00:00:02 +0700

    Fix some text

--- components/Content.vue
+++ components/Content.vue
@@ -83,7 +83,7 @@
             <div class="row">
                 <div class="col-12 work-sans">
                     <h3 class="text-center">Save in Date</h3>
-                    <p class="text-center">Yang Insya Allah akan dilaksanakan Pada</p>
+                    <p class="text-center">Yang Insya Allah akan dilaksanakan pada</p>
                 </div>
             </div>
             <div class="row">
